24ffd1faa147d74292582187aa0bf5a970da32f4
zhangqianhui <zhang163220@gmail.com>
2017-12-02 20:55:36 +0800

    update code

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ flags.DEFINE_integer("batch_size" , 64, "batch size")
 flags.DEFINE_integer("max_iters" , 600000, "the maxmization epoch")
 flags.DEFINE_integer("latent_dim" , 128, "the dim of latent code")
 flags.DEFINE_integer("learn_rate_init" , 0.0001, "the init of learn rate")
-flags.DEFINE_string("path", 'You path', "for examle, '/home/jack/data/' is the directory of your celebA(Dataset's name must be celebA)")
-flags.DEFINE_integer("op", 1, "Training or Test")
+#Please set this num of repeat by the size of your datasets.
+flags.DEFINE_integer("repeat", 10000, "the numbers of repeat for your datasets")
+flags.DEFINE_string("path", '/home/jichao/data/', "for example, '/home/jack/data/' is the directory of your celebA data")
+flags.DEFINE_integer("op", 0, "Training or Test")
 
 FLAGS = flags.FLAGS
 if __name__ == "__main__":
@@ -29,22 +31,20 @@ if __name__ == "__main__":
     batch_size = FLAGS.batch_size
     max_iters = FLAGS.max_iters
     latent_dim = FLAGS.latent_dim
+    data_repeat = FLAGS.repeat
 
     learn_rate_init = FLAGS.learn_rate_init
-
     cb_ob = CelebA(FLAGS.path)
 
-    vaeGan = vaegan(batch_size = batch_size, max_iters=max_iters,
-                      model_path = model_path, data_ob= cb_ob, latent_dim = latent_dim,
-                      sample_path = sample_path , log_dir = root_log_dir , learnrate_init=learn_rate_init)
+    vaeGan = vaegan(batch_size= batch_size, max_iters= max_iters, repeat = data_repeat,
+                      model_path= model_path, data_ob= cb_ob, latent_dim= latent_dim,
+                      sample_path= sample_path , log_dir= root_log_dir , learnrate_init= learn_rate_init)
 
     if FLAGS.op == 0:
-
         vaeGan.build_model_vaegan()
         vaeGan.train()
 
     else:
-
         vaeGan.build_model_vaegan()
         vaeGan.test()
 
--- utils.py
+++ utils.py
@@ -14,41 +14,39 @@ def mkdir_p(path):
         else:
             raise
 
-#
-# def get_image(image_path, image_size, is_crop=True, resize_w=64, is_grayscale=False):
-#     return transform(imread(image_path, is_grayscale), image_size, is_crop, resize_w)
-#
-#
-# def transform(image, npx=64, is_crop=False, resize_w=64):
-#     # npx : # of pixels width/height of image
-#     if is_crop:
-#         cropped_image = center_crop(image, npx, resize_w=resize_w)
-#     else:
-#         cropped_image = image
-#         cropped_image = scipy.misc.imresize(cropped_image,
-#                                             [resize_w, resize_w])
-#     return np.array(cropped_image) / 127.5 - 1
+def get_image(image_path, image_size, is_crop=True, resize_w=64, is_grayscale=False):
+    return transform(imread(image_path, is_grayscale), image_size, is_crop, resize_w)
 
-#
-# def center_crop(x, crop_h, crop_w=None, resize_w=64):
-#     if crop_w is None:
-#         crop_w = crop_h
-#     h, w = x.shape[:2]
-#     j = int(round((h - crop_h) / 2.))
-#     i = int(round((w - crop_w) / 2.))
-#
-#     return scipy.misc.imresize(x[40:218 - 30, 15: 178 - 15], [resize_w, resize_w])
+
+def transform(image, npx=64, is_crop=False, resize_w=64):
+    # npx : # of pixels width/height of image
+    if is_crop:
+        cropped_image = center_crop(image, npx, resize_w=resize_w)
+    else:
+        cropped_image = image
+        cropped_image = scipy.misc.imresize(cropped_image,
+                                            [resize_w, resize_w])
+    return np.array(cropped_image) / 127.5 - 1
+
+def center_crop(x, crop_h , crop_w=None, resize_w=64):
+
+    if crop_w is None:
+        crop_w = crop_h
+    h, w = x.shape[:2]
+    j = int(round((h - crop_h)/2.))
+    i = int(round((w - crop_w)/2.))
+    return scipy.misc.imresize(x[j:j+crop_h, i:i+crop_w],
+                               [resize_w, resize_w])
 
 
 def save_images(images, size, image_path):
     return imsave(inverse_transform(images), size, image_path)
 
-
-# def imread(path, is_grayscale=False):
-#     if (is_grayscale):
-#         return scipy.misc.imread(path, flatten=True).astype(np.float)
-#     else:
-#         return scipy.misc.imread(path).astype(np.float)
+def imread(path, is_grayscale=False):
+    if (is_grayscale):
+        return scipy.misc.imread(path, flatten=True).astype(np.float)
+    else:
+        return scipy.misc.imread(path).astype(np.float)
 
 
 def imsave(images, size, path):
